ea75a9406e4892a99f73ba657a87d05fae71e463
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-06 16:05:52 +0400

    feat: add menu layout and basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
-<div></div>
+  <main class="blocks">
+    <div class="blocks__container blocks-wrapper">
+      
+      <div class="menu">
+        <div class="menu__body">
+          <h2 class="menu__heading">Добавление товара</h2>
+          <div class="menu__title">
+            <label for="title" class="label-menu label-menu-required">Наименование товара</label>
+            <input class='input-menu' id="title" placeholder="Введите наименование товара">
+          </div>
+          <div class="menu__description">
+            <label for="area" class="label-menu">Описание товара</label>
+            <textarea class='input-menu' id="area" placeholder="Введите описание товара"></textarea>
+          </div>
+          <div class="menu__link">
+            <label for="link" class="label-menu label-menu-required">Наименование товара</label>
+            <input class='input-menu' id="link" placeholder="Введите ссылку">
+          </div>
+          <div class="menu__price">
+            <label for="link" class="label-menu label-menu-required">Цена товара</label>
+            <input class='input-menu' id="link" placeholder="Введите цену">
+          </div>
+        </div>
+        <button class="menu__btn">Добавить товар</button>
+      </div>
+
+      <div class="products"></div>
+    </div>
+  </main>
 </template>
 
 <script>
