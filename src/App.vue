7acd857540d5a20376d885bb31394335180e48f6
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-06 17:32:48 +0400

    feat: add product list layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="blocks">
     <div class="blocks__container blocks-wrapper">
-      
+
       <div class="menu">
         <div class="menu__body">
           <h2 class="menu__heading">Добавление товара</h2>
@@ -25,7 +25,26 @@
         <button class="menu__btn">Добавить товар</button>
       </div>
 
-      <div class="products"></div>
+      <div class="products">
+        <div class="products__container">
+          <div class="products__card card" v-for="item in 6" :key="item">
+            
+            <div class="card__picture">
+              <img src="@/assets/images/item-default.png" alt="product">
+            </div>
+
+            <div class="card__body">
+              <h3 class="card__title">Наименование товара</h3>
+
+              <p class="card__text">Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк</p>
+
+              <span class="card__price">10 000 руб.</span>
+            </div>
+
+            <div class="card__remove"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
